73967abe95301dbe63a04ce9a3158164de77a4af
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2025-08-17 19:30:36 +0300

    feat: integrate heroui provider and tailwind plugin

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
+import "@heroui/react/styles.css";
 import "./globals.css";
+import { HeroUIProvider } from "@heroui/react";
 
 export const metadata: Metadata = {
   title: "BowlBuilder",
@@ -14,7 +16,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ru">
-      <body className="antialiased">{children}</body>
+      <body className="antialiased">
+        <HeroUIProvider>{children}</HeroUIProvider>
+      </body>
     </html>
   );
 }
