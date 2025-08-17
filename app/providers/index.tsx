42d1befe70abd770143dcc08d2ff8b8aca758619
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2025-08-17 19:41:10 +0300

    fix: remove heroui styles import

--- app/providers/index.tsx
+++ app/providers/index.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import type { ReactNode } from 'react';
-import '@heroui/react/styles.css';
 import { HeroUIProvider } from '@heroui/react';
 
 export function Providers({ children }: { children: ReactNode }) {
