b621f182da574867d3a083fb38dc49e318750e34
Oleg Kipchatov <o.kipchatov@tinkoff.ru>
2025-08-19 09:59:08 +0300

    feat: init new app

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,55 @@
-import type { Metadata } from "next";
-import "./globals.css";
+import "@/styles/globals.css";
+import { Metadata, Viewport } from "next";
+import { Link } from "@heroui/link";
+import clsx from "clsx";
+
+import { Providers } from "./providers";
+
+import { siteConfig } from "@/config/site";
+import { fontSans } from "@/config/fonts";
+import { Navbar } from "@/components/navbar";
 
 export const metadata: Metadata = {
-  title: "BowlBuilder",
-  description:
-    "Сервис для создания и сохранения собственных миксов для кальянных чаш",
+  title: {
+    default: siteConfig.name,
+    template: `%s - ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
 };
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
-    <html lang="ru">
-      <body className="antialiased">{children}</body>
+    <html suppressHydrationWarning lang="en">
+      <head />
+      <body
+        className={clsx(
+          "min-h-screen text-foreground bg-background font-sans antialiased",
+          fontSans.variable,
+        )}
+      >
+        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
+          <div className="relative flex flex-col h-screen">
+            <Navbar />
+            <main className="container mx-auto max-w-7xl flex-grow">
+              {children}
+            </main>
+          </div>
+        </Providers>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,27 @@
-import HomePage from "@/pages/home";
-
 export default function Home() {
-  return <HomePage />;
+  return (
+    <section className="flex flex-col items-center justify-center text-center p-4">
+      <h1 className="text-5xl font-bold">BowlBuilder</h1>
+      <h2 className="mt-4 text-lg max-w-xl">
+        Сервис для создания и сохранения собственных миксов для кальянных чаш
+      </h2>
+      <p className="mt-8 text-sm font-medium tracking-wide text-gray-500">
+        Создайте свою идеальную чашу.
+      </p>
+      <p className="mt-6 text-sm">
+        Подписывайтесь на{
+          " "
+        }
+        <a
+          href="https://t.me/+BjywrBIpCoQ1YTA6"
+          className="underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          канал разработки
+        </a>{" "}
+        и следите за обновлениями.
+      </p>
+    </section>
+  );
 }
