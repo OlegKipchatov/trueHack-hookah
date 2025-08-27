1a855800bc42a5c1c34c57deb34d0292e9a4f380
Oleg Kipchatov <o.kipchatov@tinkoff.ru>
2025-08-27 18:54:47 +0300

    feat: replace link to next link

--- src/widgets/navbar/ui/navbar.tsx
+++ src/widgets/navbar/ui/navbar.tsx
@@ -1,10 +1,11 @@
+import NextLink from "next/link";
+
 import {
   HeroUINavbar,
   NavbarContent,
   NavbarMenuToggle,
   NavbarBrand,
   NavbarItem,
-  Link,
 } from "@heroui/react";
 
 import { ThemeSwitch } from "@/features/theme-switch";
@@ -15,10 +16,10 @@ export const Navbar = () => {
     <HeroUINavbar maxWidth="xl" position="sticky">
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
-          <Link className="flex justify-start items-center gap-1" href="/">
+          <NextLink className="flex justify-start items-center gap-1" href="/" >
             <Logo />
             <p className="font-bold text-inherit">BowlBuilder</p>
-          </Link>
+          </NextLink>
         </NavbarBrand>
       </NavbarContent>
 
