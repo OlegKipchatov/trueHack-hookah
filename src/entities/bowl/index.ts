b682633c991b14952b7dbc1bb135b33288844db1
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2025-08-31 16:17:14 +0300

    feat: add optional edit handler to BowlCard

--- src/entities/bowl/index.ts
+++ src/entities/bowl/index.ts
@@ -1,3 +1,4 @@
 export { useBowls } from "./model/bowl";
 export type { Bowl, BowlTobacco } from "./model/bowl";
 export { BowlCard } from "./ui/bowl-card";
+export type { BowlCardProps } from "./ui/bowl-card";
