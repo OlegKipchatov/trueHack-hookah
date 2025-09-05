7c17b0885e9a1873cfb08061eb97ff380e052ef1
Oleg Kipchatov <35765711+OlegKipchatov@users.noreply.github.com>
2025-09-05 16:38:49 +0300

    feat: add bowl edit page (#45)
    Co-authored-by: Oleg Kipchatov <o.kipchatov@tinkoff.ru>

---
+++ app/bowls/edit/page.tsx
@@ -0,0 +1,53 @@
+"use client";
+
+import { useSearchParams, useRouter } from "next/navigation";
+import { Alert, Modal, ModalContent, ModalHeader } from "@heroui/react";
+import { Suspense } from "react";
+
+import { BowlForm } from "@/features/upsert-bowl/ui/bowl-form";
+import { useBowls, type Bowl } from "@/entities/bowl";
+
+export type EditBowlPageProps = {};
+
+const Edit = () => {
+  const searchParams = useSearchParams();
+  const id = searchParams.get("id");
+  const { bowls, updateBowl } = useBowls();
+  const router = useRouter();
+
+  const bowl = bowls.find((b) => b.id === id);
+
+  const handleSubmit = (b: Bowl) => {
+    updateBowl(b);
+    router.push("/user");
+  };
+
+  if (!bowl) {
+    return (
+      <div className="p-6">
+        <Alert color="danger" variant="solid">
+          Чаша для редактирования не найдена
+        </Alert>
+      </div>
+    );
+  }
+
+  return (
+    <Modal defaultOpen hideCloseButton isDismissable={false} motionProps={{}}>
+      <ModalContent>
+        <ModalHeader>Edit Bowl</ModalHeader>
+        <BowlForm bowl={bowl} onSubmit={handleSubmit} />
+      </ModalContent>
+    </Modal>
+  );
+};
+
+const EditBowlPage = ({}: EditBowlPageProps) => {
+  return (
+    <Suspense>
+      <Edit />;
+    </Suspense>
+  );
+};
+
+export default EditBowlPage;
