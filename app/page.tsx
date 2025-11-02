81f83fdf83a5d3534a5f2d4fe820a28828a3110a
Oleg Kipchatov <o.kipchatov@tinkoff.ru>
2025-11-02 17:05:23 +0300

    Revert "Use rich translation for follow link"
    This reverts commit f12582814cc12946f52615be7d839e31b55c59d2.

--- app/page.tsx
+++ app/page.tsx
@@ -19,18 +19,16 @@ const HomePage = ({}: HomePageProps) => {
         {translate("app.cta")}
       </p>
       <p className="mt-6 text-sm">
-        {translate.rich("app.follow", {
-          link: (chunks) => (
-            <Link
-              className="underline"
-              href="https://t.me/+BjywrBIpCoQ1YTA6"
-              rel="noopener noreferrer"
-              target="_blank"
-            >
-              {chunks}
-            </Link>
-          ),
-        })}
+        {translate("app.followPrefix")}{" "}
+        <Link
+          className="underline"
+          href="https://t.me/+BjywrBIpCoQ1YTA6"
+          rel="noopener noreferrer"
+          target="_blank"
+        >
+          {translate("app.followLink")}
+        </Link>{" "}
+        {translate("app.followSuffix")}
       </p>
     </Page>
   );
